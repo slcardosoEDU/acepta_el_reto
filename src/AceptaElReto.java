9338efa25ce02e58bf58ca85a056dedadb69c674
Samuel <slcardoso.docente@gmail.com>
2022-12-04 22:39:07 +0100

    Subo estructura inicial del proyecto. Se excluyen del proyecto todos los ficheros del IDE, quedando unicamente src y test.

---
+++ src/AceptaElReto.java
@@ -0,0 +1,55 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Samuel Loureiro Cardoso
+ */
+public class AceptaElReto {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner sc = getScanner();
+        //Este bucle hay que modificarlo en función del formato de la entrada.
+        do{
+            String cp = sc.nextLine();
+            System.out.println(resolverCasoPrueba(cp));
+            
+        }while(sc.hasNext());
+
+    }
+    /** * 
+     * Establece la entrada al fichero indicado en Este método permite leer línea a línea un fichero en lugar de tener que leer porconsola.la ruta.consola.
+     * @param rutaFichero Path de la ubicació  del fichero en el sistema.
+
+     */
+    public static Scanner getScanner(String rutaFichero){
+        Scanner sc = null;
+        try{
+           File text = new File(rutaFichero);
+           sc = new Scanner(text);
+        }catch(FileNotFoundException e){
+            System.err.println(e);
+        }
+        return sc;        
+    }
+    
+    /**
+     * Establece la entrada estándar.
+     * Este es el método que se debe utilizar para entregar los problemas.
+     * @return Un objeto Scanner asociado al terminal del sistema.
+     */
+    public static Scanner getScanner(){
+        return new Scanner(System.in);        
+    }
+
+    
+    public static  String resolverCasoPrueba(String cp){
+        //TODO AQUI RESOLVEMOS EL PROBLEMA
+        String salida = null;
+        return salida;
+    }
+}
